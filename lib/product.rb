795929d917931b8ad29a84a10a259378b12bdbd9
Keith Pitty <keith@keithpitty.com>
2021-06-06 11:18:30 +1000

    Allow for basic sales tax
    We introduce logic to allow for exemptions and calculate the price with
    a 10% sales tax applied for products that are not exempt.

--- lib/product.rb
+++ lib/product.rb
@@ -6,6 +6,18 @@ class Product
   end
 
   def price_with_taxes
-    @price
+    return @price if exempt_from_sales_tax?
+
+    price_with_sales_tax
+  end
+
+  private
+
+  def exempt_from_sales_tax?
+    @description == 'book'
+  end
+
+  def price_with_sales_tax
+    (@price * 1.1).round(2)
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,9 +3,14 @@ require 'product'
 RSpec.describe Product do
   describe "#price_with_taxes" do
     let(:book) { Product.new(description: 'book', price: 12.49) }
+    let(:music_cd) { Product.new(description: 'music CD', price: 14.99) }
 
     it "returns correct price for local book" do
       expect(book.price_with_taxes).to eq(12.49)
+    end
+
+    it "returns correct price for local music CD" do
+      expect(music_cd.price_with_taxes).to eq(16.49)
     end
   end
 end
